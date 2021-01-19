66402e90f1d6b22b3d20ba7118b2bfa8a90a6a57
Durrell Jules <dujules23@gmail.com>
2021-01-19 18:14:28 -0500

    imported axios, yup, and form.js, put form component into jsx

---
+++ onboardingapp/src/App.js
@@ -0,0 +1,21 @@
+//Axios import
+import axios from 'axios'
+
+//yup import
+import * as yup from 'yup'
+
+//imporing form.js
+import Form from './Form'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
