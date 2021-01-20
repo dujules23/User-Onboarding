0b3da53b2a0974d2b7b9d55dcbab9eb663bf777a
Durrell Jules <dujules23@gmail.com>
2021-01-19 22:54:12 -0500

    schema and slice of state added for errors

--- onboardingapp/src/App.js
+++ onboardingapp/src/App.js
@@ -1,5 +1,5 @@
 //Imported React
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 //Axios import
 import axios from 'axios'
@@ -11,23 +11,67 @@ import * as yup from 'yup'
 import Form from './Form'
 
 
+ //Schema - shape of the form
+ const schema = yup.object().shape({
 
+  name: yup.string().required('Name is Required'),
+  email: yup.string().required('Email is Required'),
+  password: yup.string().required('Password is Required').min(6, 'Needs to be six characters minimum'),
+  terms: yup.boolean().oneOf([true], 'You Must Read the Terms of Service')
+})
+
+// console.log(schema)
 
 function App() {
-  //Slice of State
+  
+  //Slice of State for the form
   const [form, setForm] = useState({
     name: '',
     email: '',
     password:'',
     terms: false,
-  })
+    })
+  
 
+  //Slice of state for errors
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    password:'',
+    terms: false,
+    })
+
+
+  //Slice of state for Button (disabled until all fields of the form have been completed)
+  const [disabled, setDisabled] = useState(true)
+  
+  // console.log(disabled)
+
+  const validate = (name, value) => {
+
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setErrors({...errors, [name]: ''}))
+      .catch(err => setErrors({...errors, [name]: err.errors[0]}))
+  }
+ 
+
+  useEffect(() =>{
+    schema.isValid(form).then(valid =>  setDisabled(!valid))
+  }, [form])
 
   return (
 
     <div className="App">
       <h1>Onboarding</h1>
-      <Form/>
+      <Form
+        form={form}
+        setForm={setForm}
+        disabled={disabled}
+        setErrors={validate}
+
+
+      />
     </div>
   );
 }
--- onboardingapp/src/Form.js
+++ onboardingapp/src/Form.js
@@ -1,14 +1,50 @@
 //React import
 import React from 'react'
 
+
+
 export default function Form (props) {
-    return(
+
+    
+
+    const { form, setForm, setErrors, disabled } = props
+
+    // console.log (values)
+
+    //function that allows inputs to be changed
+    const change = e => {
+
+        //target
+        const { checked, type, name, value } = e.target
         
+        // //indicates unique checkbox feature that says 'if the type is a checkbox, checked populate, otherwise it will go back to the original state (false)'
+        const valueToUse = type === 'checkbox' ? checked : value
+        
+        setForm({...form, [name]: valueToUse})
+        
+        //
+        setErrors(name,valueToUse)
+        
+    }
+   
+
+    // const submit = e => {
+
+
+    // }
+
+
+    return(
+
         <form className="form-container">
             <label>Name:
                 <input
                     name='name'
                     type="text"
+                    placeholder='Enter Name'
+                    maxLength='30'
+                    value={form.name}
+                    onChange={change}
                 />
             </label>
             <br/>
@@ -16,6 +52,10 @@ export default function Form (props) {
                 <input
                     name="email"
                     type="text"
+                    placeholder='Enter Email'
+                    maxLength='30'
+                    value={form.email}
+                    onChange={change}
                 />
             </label>
             <br/>
@@ -23,17 +63,23 @@ export default function Form (props) {
                 <input
                     name="password"
                     type="text"
+                    placeholder='Enter Password'
+                    maxLength='10'
+                    value={form.password}
+                    onChange={change}
                 />
             </label>
             <br/>
             <label>Terms of Service:
-                <input
+                <input                                                     
                     name="terms"
                     type="checkbox"
+                    checked={form.terms}
+                    onChange={change}
                 />
             </label>
             <br/>
-            <button>Submit</button>
+            <button disabled={disabled} >Submit</button>
         </form>
     )
     
